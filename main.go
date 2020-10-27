45e05f18ce917d77883ffc59f5fd7f41909deb49
1139556759@qq.com <1139556759@qq.com>
2020-10-27 22:35:44 +0800

    read & process

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
 
 type LogProcess struct {
-	rc chan string
-	wc chan string
+	rc chan []byte
+	wc chan *Message
 	r  Reader
 	w  Write
 }
 
+type Message struct {
+	TimeLocal                    time.Time
+	ByteSent                     int
+	Path, Method, Scheme, Status string
+	UpstreamTime, RequestTime    float64
+}
+
 type Reader interface {
-	read(rc chan string)
+	read(rc chan []byte)
 }
 
 type Write interface {
-	write(wc chan string)
+	write(wc chan *Message)
 }
 
 type ReadFromFile struct {
@@ -29,35 +43,113 @@ type WriteToInfluxDB struct {
 	influxDb string
 }
 
+//TODO 写入到MongoDB
+//type WriteToMongoDB struct {
+//	mongoConfig string
+//}
+
 // 读取模块
-func (r *ReadFromFile) read(rc chan string) {
-	rc <- "hello world"
+func (r *ReadFromFile) read(rc chan []byte) {
+	// 读取文件
+	file, err := os.Open(r.path)
+	if err != nil {
+		fmt.Printf("os.Open() fail, err: %v\n", err.Error())
+		return
+	}
+	file.Seek(0, 2)
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			fmt.Printf("reader.ReadBytes() fail, err : %v\n", err.Error())
+			return
+		}
+		rc <- line[:len(line)-1]
+	}
+
 }
 
 // 解析模块
 func (l *LogProcess) process() {
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	reg := `([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`
+	r := regexp.MustCompile(reg)
+	for v := range l.rc {
+		ret := r.FindStringSubmatch(string(v))
+		if len(ret) != 14 {
+			fmt.Printf("reg FindStringSubmatch(),err: %v \n", string(v))
+			continue
+		}
+		message := &Message{}
+		loc, _ := time.LoadLocation("Asia/Shanghai")
+		locationTime, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc)
+		if err != nil {
+			fmt.Printf("time.ParseInLocation fail, err : %v \n", ret[4])
+			continue
+		}
+		// 172.0.0.12 - - [04/Mar/2018:13:49:52 +0000] http "GET /foo?query=t HTTP/1.0" 200 2133 "-" "KeepAliveClient" "-" 1.005 1.854
+		message.TimeLocal = locationTime
+		byteSent, err := strconv.Atoi(ret[8])
+		if err != nil {
+			fmt.Printf("strconv.Atoi() fail,err: %v \n", ret[8])
+			continue
+		}
+		message.ByteSent = byteSent
+
+		// "GET /foo?query=t HTTP/1.0"
+		splitSlice := strings.Split(ret[6], " ")
+		if len(splitSlice) != 3 {
+			fmt.Printf("strings.Split fail,err: %v\n", splitSlice)
+			continue
+		}
+		message.Method = splitSlice[0]
+		urlPath, err := url.Parse(splitSlice[1])
+		if err != nil {
+			log.Printf("urlPath.Parse() fail,err: %v \n", splitSlice[1])
+			continue
+		}
+		message.Path = urlPath.Path
+		message.Scheme = ret[5]
+		message.Status = ret[7]
+		upstreamTime, _ := strconv.ParseFloat(ret[12], 64)
+		message.UpstreamTime = upstreamTime
+		requestTime, _ := strconv.ParseFloat(ret[13], 64)
+		message.RequestTime = requestTime
+		l.wc <- message
+	}
 }
 
 // 写入模块->influxDB
-func (w *WriteToInfluxDB) write(wc chan string) {
-	fmt.Println("读取到的数据是: ", <-wc)
+func (w *WriteToInfluxDB) write(wc chan *Message) {
+	for v := range wc {
+		fmt.Println(v)
+	}
 }
+
+//TODO 写入模块->MongoDB
+//func (w *WriteToMongoDB) write(wc chan string) {
+//	fmt.Println("写入到MongoDB的数据是: ", <-wc)
+//}
 
 func main() {
 	readFromFile := &ReadFromFile{
-		path: "",
+		path: "./file/access.log",
 	}
-	wToInfluxDB := &WriteToInfluxDB{}
-	l := &LogProcess{
-		rc: make(chan string),
-		wc: make(chan string),
+	wToInfluxDB := &WriteToInfluxDB{
+		influxDb: "",
+	}
+	//TODO 写入到MongoDB
+	//wToMongoDB := &WriteToMongoDB{mongoConfig: ""}
+	lp := &LogProcess{
+		rc: make(chan []byte, 200),
+		wc: make(chan *Message, 200),
 		r:  readFromFile,
 		w:  wToInfluxDB,
 	}
-	go readFromFile.read(l.rc)
-	go l.process()
-	go wToInfluxDB.write(l.wc)
-	time.Sleep(time.Second * 1)
+	go lp.r.read(lp.rc)
+	go lp.process()
+	go lp.w.write(lp.wc)
+	time.Sleep(time.Second * 60)
 }
--- mock/mockData.go
+++ mock/mockData.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
 func main() {
+	file, err := os.OpenFile("file/access.log", os.O_WRONLY|os.O_APPEND, 0777)
+	if err != nil {
+		fmt.Printf("os.OpenFile() fail,err: %v \n", err.Error())
+		return
+	}
+	defer file.Close()
+	for {
+		for i := 0; i < 4; i++ {
+			now := time.Now()
+			rand.Seed(now.UnixNano())
+			paths := []string{"/foo", "/bar", "/baz", "/qux", "/foo", "/bar", "/bar", "/bar"}
+			path := paths[rand.Intn(len(paths))]
+			requestTime := rand.Float64()
+			if path == "/foo" {
+				requestTime = requestTime + 1.4
+			}
+
+			scheme := "http"
+			if now.UnixNano()/1000%2 == 1 {
+				scheme = "https"
+			}
+			dateTime := now.Format("02/Jan/2006:15:04:05")
+			code := 200
+			if now.Unix()%10 == 1 {
+				code = 500
+			}
+			bytesSend := rand.Intn(1000) + 500
+			if path == "/foo" {
+				bytesSend = bytesSend + 1000
+			}
+			line := fmt.Sprintf("172.0.0.12 - - [%s +0000] %s \"GET %s HTTP/1.0\" %d %d \"-\" \"KeepAliveClient\" \"-\" - %.3f\n", dateTime, scheme, path, code, bytesSend, requestTime)
+			_, err := file.Write([]byte(line))
+			if err != nil {
+				log.Println("writeToFile error:", err)
+			}
+		}
+		time.Sleep(time.Millisecond * 200)
+	}
 }
